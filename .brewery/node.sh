9cab49195ab30510c39a253efb90f84b923a82b9
Nathan Kot <me@nathankot.com>
2013-06-21 11:05:04 +1200

    Move ruby brews to ruby.sh

--- .brewery/node.sh
+++ .brewery/node.sh
@@ -1,2 +1,4 @@
+brew install node
+
 # CoffeeScript via NPM
 sudo npm install -g coffee-script
--- .brewery/ruby.sh
+++ .brewery/ruby.sh
@@ -1,2 +1,7 @@
+brew install openssl
+brew install rbenv
+brew install ruby-build
+brew install rails-completion
+
 git clone https://github.com/sstephenson/rbenv-default-gems.git ~/.rbenv/plugins/rbenv-default-gems
 git clone git://github.com/tpope/rbenv-ctags.git ~/.rbenv/plugins/rbenv-ctags
