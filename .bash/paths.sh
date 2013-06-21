b336f2b3463e9a41f469b97b8ced2501bbbc77a4
Nathan Kot <me@nathankot.com>
2013-06-22 09:43:19 +1200

    Update python path

--- .bash/paths.sh
+++ .bash/paths.sh
@@ -4,4 +4,4 @@ export PATH="/usr/local/bin:/usr/local/sbin:$PATH"
 # Add ~/bin to path
 export PATH="$HOME/bin:$PATH"
 
-export PYTHONPATH="$(brew --prefix)/lib/python2.7/site-packages:$PYTHONPATH"
+export PYTHONPATH="/usr/local/lib/python2.7/site-packages:$PYTHONPATH"
