393cd67337ef5b7bfe8a005eaef341658ab7fec3
Nathan Kot <me@nathankot.com>
2013-06-22 09:30:43 +1200

    Fix powerline for tmux.

--- .bash/paths.sh
+++ .bash/paths.sh
@@ -3,3 +3,5 @@ export PATH="/usr/local/bin:/usr/local/sbin:$PATH"
 
 # Add ~/bin to path
 export PATH="$HOME/bin:$PATH"
+
+export PYTHONPATH="$(brew --prefix)/lib/python2.7/site-packages:$PYTHONPATH"
