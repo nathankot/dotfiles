462e1e622207efb979fa9a1aed79122dcfef4935
Nathan Kot <me@nathankot.com>
2013-06-24 16:48:56 +1200

    Add python scripts to the path so that powerline works.

--- .bash/paths.sh
+++ .bash/paths.sh
@@ -1,7 +1,9 @@
-# The PATH, assumes homebrew
-export PATH="/usr/local/bin:/usr/local/sbin:$PATH"
+# Python scripts (For Powerline)
+export PATH="/usr/local/share/python:$PATH"
+export PYTHONPATH="/usr/local/lib/python2.7/site-packages:$PYTHONPATH"
 
 # Add ~/bin to path
 export PATH="$HOME/bin:$PATH"
 
-export PYTHONPATH="/usr/local/lib/python2.7/site-packages:$PYTHONPATH"
+# Homebrew executables
+export PATH="/usr/local/bin:/usr/local/sbin:$PATH"
