87412d14c3f6f0101d33392166d2359ae3f996cc
Nathan Kot <me@nathankot.com>
2013-06-25 13:11:28 +1200

    Tweak the prompt.

--- .shell/zsh-theme.sh
+++ .shell/zsh-theme.sh
@@ -7,9 +7,7 @@ ZSH_THEME_GIT_PROMPT_PREFIX="${make_yellow}‹"
 ZSH_THEME_GIT_PROMPT_SUFFIX="›${reset_color}"
 
 if which rbenv &> /dev/null; then
-  RPS1='$(git_prompt_info)%{$fg[red]%} ‹`rbenv local | sed -e "s/ (set.*$//"`›%{$reset_color%}'
-else
-  RPS1='$(git_prompt_info)'
+  RPS1='%{$fg[red]%}‹`rbenv version | sed -e "s/ (set.*$//"`›%{$reset_color%}'
 fi
 
-PROMPT='${background_processes}%{$fg[magenta]%}%m%{$reset_color%} %2~ %B» %b '
+PROMPT='${background_processes}%{$fg[magenta]%}%m%{$reset_color%} %2~ $(git_prompt_info)%B» %b '
