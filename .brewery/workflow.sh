8a2e6e887118de5f006ca71fa59a10d9d321cf3e
Nathan Kot <me@nathankot.com>
2013-06-30 10:23:35 +1200

    Add the phantomjs brew.

--- .brewery/workflow.sh
+++ .brewery/workflow.sh
@@ -5,3 +5,6 @@ brew install reattach-to-user-namespace --wrap-pbcopy-and-pbpaste
 brew install macvim --override-system-vim
 brew install ack
 brew install ctags
+
+# For spec-ing
+brew install phantomjs
