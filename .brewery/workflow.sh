8c07277fdb4df0c8c48241051b03fd65a590e867
Nathan Kot <me@nathankot.com>
2013-08-14 12:09:35 +1200

    Install silver searcher.

--- .brewery/workflow.sh
+++ .brewery/workflow.sh
@@ -5,6 +5,7 @@ brew install reattach-to-user-namespace --wrap-pbcopy-and-pbpaste
 # Want to compile against homebrew's Python.
 brew install macvim --override-system-vim
 brew install ack
+brew install the_silver_searcher
 brew install ctags
 
 # For spec-ing
