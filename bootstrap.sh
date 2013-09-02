27719bf375e3d125f2b793a82a5a3716e0fc2089
Nathan Kot <me@nathankot.com>
2013-09-02 13:47:54 +1200

    Fix up bootstrap command.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,26 +2,20 @@
 
 cd "$(dirname "${BASH_SOURCE}")"
 
-function doIt() {
+if [ $1 == 'update' ]; then
+  rsync -av ~/.tmuxinator .
+  rsync -av ~/lib/iterm2/com.googlecode.iterm2.plist ./lib/iterm2/com.googlecode.iterm2.plist
+  rsync -av ~/Library/Preferences/org.pqrs.KeyRemap4MacBook.plist ./Library/Preferences/org.pqrs.KeyRemap4MacBook.plist
+fi
 
-  if [$1 == 'update']; then
-    rsync -av ~/.tmuxinator .
-    rsync -av ~/lib/iterm2/com.googlecode.iterm2.plist ./lib/iterm2/com.googlecode.iterm2.plist
-    rsync -av ~/Library/Preferences/org.pqrs.KeyRemap4MacBook.plist ./Library/Preferences/org.pqrs.KeyRemap4MacBook.plist
-  fi
-
-  rsync --exclude ".git/" \
-        --exclude ".DS_Store" \
-        --exclude "bootstrap.sh" \
-        --exclude "install.sh" \
-        --exclude "brew.sh" \
-        --exclude "osx.sh" \
-        --exclude "README.md" \
-        -av --no-perms . ~
-}
-
-doIt
-unset doIt
+rsync --exclude ".git/" \
+      --exclude ".DS_Store" \
+      --exclude "bootstrap.sh" \
+      --exclude "install.sh" \
+      --exclude "brew.sh" \
+      --exclude "osx.sh" \
+      --exclude "README.md" \
+      -av --no-perms . ~
 
 if [ ! -d "$HOME/.vim/bundle/vundle" ]; then
   git clone https://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
