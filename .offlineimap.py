65c2e7ef0b4b1e99554d0f733e447f3f366dd68d
Nathan Kot <me@nathankot.com>
2013-09-07 22:31:49 +1200

    Don't need to sudo -u

---
+++ .offlineimap.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import re, subprocess
+from subprocess import call
+
+def cert_file():
+  return call(["echo", "$SSL_CERT_FILE"])
+
+def get_keychain_pass(account=None, server=None):
+    params = {
+        'security': '/usr/bin/security',
+        'command': 'find-internet-password',
+        'account': account,
+        'server': server,
+        'keychain': '/Users/nathan/Library/Keychains/login.keychain',
+    }
+    command = "%(security)s -v %(command)s -g -a %(account)s -s %(server)s %(keychain)s" % params
+    output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
+    outtext = [l for l in output.splitlines()
+               if l.startswith('password: ')][0]
+
+    return re.match(r'password: "(.*)"', outtext).group(1)
