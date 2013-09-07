51b7ff37dd91571e4a76442f52983f5970636fac
Nathan Kot <me@nathankot.com>
2013-09-08 11:49:49 +1200

    Start dropbox on login again.

--- osx.sh
+++ osx.sh
@@ -106,6 +106,7 @@ sudo touch /private/var/db/.AccessibilityAPIEnabled
 ### LOGIN ITEMS (Need to run this after installation.)
 osascript -e "tell app \"System Events\"
                 make login item at end with properties {path:\"$HOME/Applications/Amethyst.app\", hidden:true}
+                make login item at end with properties {path:\"$HOME/Applications/Dropbox.app\", hidden:true}
                 make login item at end with properties {path:\"$HOME/Applications/Shortcat.app\", hidden:true}
                 make login item at end with properties {path:\"$HOME/Applications/Flux.app\", hidden:true}
                 make login item at end with properties {path:\"$HOME/Applications/Alfred 2.app\", hidden:true}
