ae40378942223a99c25ed9032a69456061ff2fee
Nathan Kot <me@nathankot.com>
2013-12-21 13:36:42 +1300

    Ignore generated completions.

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,7 +12,7 @@ if [ "$1" == "update" ]; then
   rsync -av ~/.gitconfig .
   rsync -av ~/.git_template .
   rsync -av ~/.vim/snippets ./.vim/
-  rsync -av ~/.config/fish ./.config/ --exclude "fish_history"
+  rsync -av ~/.config/fish ./.config/ --exclude "fish_history" --exclude "generated_completions"
   rsync -av ~/lib/iterm2/com.googlecode.iterm2.plist ./lib/iterm2/com.googlecode.iterm2.plist
 fi
 
