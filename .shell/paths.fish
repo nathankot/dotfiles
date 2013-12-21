0454e190cb30646ecdb3064aaea6b2a0134efa7b
Nathan Kot <me@nathankot.com>
2013-12-21 13:23:29 +1300

    Fix paths.

--- .shell/paths.fish
+++ .shell/paths.fish
@@ -5,13 +5,13 @@ set -x PYTHONPATH /usr/local/lib/python2.7/site-packages $PYTHONPATH
 set -x PATH ./bin $HOME/bin $PATH
 
 # Homebrew executables
-set -x PATH /usr/local/bin:/usr/local/sbin $PATH
+set -x PATH /usr/local/bin /usr/local/sbin $PATH
 
 # Heroku toolbelt
 set -x PATH /usr/local/heroku/bin $PATH
 
 # NPM
-set -x PATH ./node_modules/.bin:/usr/local/share/npm/bin $PATH
+set -x PATH ./node_modules/.bin /usr/local/share/npm/bin $PATH
 
 # RBENV
 set -x PATH $HOME/.rbenv/bin $PATH
