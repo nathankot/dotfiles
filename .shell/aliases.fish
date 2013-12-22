e27ea3f2aeab09bbe3ec858f24b117b104dacefe
Nathan Kot <me@nathankot.com>
2013-12-22 16:26:57 +1300

    Prefer fish abbreviations over git aliases.

--- .shell/aliases.fish
+++ .shell/aliases.fish
@@ -1,4 +1,11 @@
-alias mux tmuxinator
-alias issues ghi
-alias l ls
-alias g git
+set fish_user_abbreviations $fish_user_abbreviations 'mux=tmuxinator'
+set fish_user_abbreviations $fish_user_abbreviations 'issues=ghi'
+set fish_user_abbreviations $fish_user_abbreviations 'l=ls'
+
+set fish_user_abbreviations $fish_user_abbreviations 'g=git'
+set fish_user_abbreviations $fish_user_abbreviations 'gs=git status'
+set fish_user_abbreviations $fish_user_abbreviations 'gd=git diff'
+set fish_user_abbreviations $fish_user_abbreviations 'gc=git commit -am '
+set fish_user_abbreviations $fish_user_abbreviations 'gco=git checkout'
+set fish_user_abbreviations $fish_user_abbreviations 'ga=git add -A'
+
