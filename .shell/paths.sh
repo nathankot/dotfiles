964b5f224013414ec5634ceadcf024886d678252
Nathan Kot <me@nathankot.com>
2014-03-22 16:20:36 +1300

    Also need to update paths.sh

--- .shell/paths.sh
+++ .shell/paths.sh
@@ -12,3 +12,9 @@ export PATH="/usr/local/heroku/bin:$PATH"
 
 # NPM
 export PATH="./node_modules/.bin:/usr/local/share/npm/bin:$PATH"
+
+# RBENV
+eval "$(rbenv init -)"
+
+# PHP
+export PATH="$(brew --prefix josegonzalez/php/php55)/bin:$PATH"
