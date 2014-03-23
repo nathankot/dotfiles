4b9960d646cd02c2479cbd4d1089cdbc395a6e38
Nathan Kot <me@nathankot.com>
2014-03-23 15:09:38 +1300

    Set LEDGER

--- .shell/environments.fish
+++ .shell/environments.fish
@@ -2,12 +2,12 @@
 
 set -x RBENV_ROOT ~/.rbenv
 
-###
-
-
 ### Browser testing
 
 set CHROME_BIN /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome
 set OPERA_BIN ~/Applications/Opera.app/Contents/MacOS/Opera
 
-###
+
+### Ledger
+
+set -x LEDGER ~/Documents/Personal/Accounts/current.ledger
