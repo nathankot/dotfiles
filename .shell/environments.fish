d451baa7f69058acbee57d0621a80566e2997af2
Nathan Kot <me@nathankot.com>
2014-04-06 11:37:27 +1200

    Set chrome bin.

--- .shell/environments.fish
+++ .shell/environments.fish
@@ -4,7 +4,7 @@ set -x RBENV_ROOT ~/.rbenv
 
 ### Browser testing
 
-set CHROME_BIN /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome
+set CHROME_BIN '~/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
 set OPERA_BIN ~/Applications/Opera.app/Contents/MacOS/Opera
 
 ### Ledger
