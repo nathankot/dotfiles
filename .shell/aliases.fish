20788f716eae1f6642c04f77993b661a41c3d7ee
Nathan Kot <me@nathankot.com>
2014-05-06 08:06:20 +1200

    John-Ledger.

--- .shell/aliases.fish
+++ .shell/aliases.fish
@@ -1,6 +1,6 @@
 set fish_user_abbreviations $fish_user_abbreviations 'mux=tmuxinator'
 set fish_user_abbreviations $fish_user_abbreviations 'issues=ghi'
-set fish_user_abbreviations $fish_user_abbreviations 'l=ledger'
+set fish_user_abbreviations $fish_user_abbreviations 'jl=ledger'
 set fish_user_abbreviations $fish_user_abbreviations 'hl=hledger'
 
 set fish_user_abbreviations $fish_user_abbreviations 'g=git'
