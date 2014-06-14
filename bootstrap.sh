0ffadd08da397b34ce4be6811f39b681d8dff818
Nathan Kot <me@nathankot.com>
2014-06-15 02:33:01 +1200

    Update and sync my own keybind settings.

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,6 +16,7 @@ if [ "$1" == "update" ]; then
   rsync -av ~/.config/fish ./.config/ --exclude "fish_history" --exclude "generated_completions"
   rsync -av ~/lib/iterm2/com.googlecode.iterm2.plist ./lib/iterm2/com.googlecode.iterm2.plist
   rsync -av ~/Library/Preferences/org.pqrs.KeyRemap4MacBook.plist ./Library/Preferences/org.pqrs.KeyRemap4MacBook.plist
+  rsync -av ~/Library/Application\ Support/KeyRemap4MacBook/private.xml ./Library/Application\ Support/KeyRemap4MacBook/private.xml
 fi
 
 rsync --exclude ".git/" \
