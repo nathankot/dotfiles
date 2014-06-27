a25598a9d47707c24c21cb45b1838797b7e7e49f
Nathan Kot <me@nathankot.com>
2014-06-27 12:46:51 +1200

    I found chrome bin being overriden in private.

--- .shell/environments.fish
+++ .shell/environments.fish
@@ -4,7 +4,7 @@ set -x RBENV_ROOT ~/.rbenv
 
 ### Browser testing
 
-set -x CHROME_BIN ~/Applications/Google\\ Chrome.app/Contents/MacOS/Google\\ Chrome
+set -x CHROME_BIN ~/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome
 set -x OPERA_BIN ~/Applications/Opera.app/Contents/MacOS/Opera
 
 ### Ledger
