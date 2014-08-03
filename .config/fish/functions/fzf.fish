4847cf81b00ba8bc1942ea5793093fe3f0330b09
Nathan Kot <me@nathankot.com>
2014-08-04 08:42:45 +1200

    Update fzf.

--- .config/fish/functions/fzf.fish
+++ .config/fish/functions/fzf.fish
@@ -1,3 +1,3 @@
 function fzf
-  fzf $argv
+  /usr/bin/ruby --disable-gems /usr/local/Cellar/fzf/0.8.6/fzf $argv
 end
--- .config/fish/functions/fzf_key_bindings.fish
+++ .config/fish/functions/fzf_key_bindings.fish
@@ -1,44 +1,56 @@
 function fzf_key_bindings
-  function __fzf_select
-    find * -path '*/\.*' -prune \
+  # Due to a bug of fish, we cannot use command substitution,
+  # so we use temporary file instead
+  if [ -z "$TMPDIR" ]
+    set -g TMPDIR /tmp
+  end
+
+  function __fzf_list
+    command find * -path '*/\.*' -prune \
       -o -type f -print \
       -o -type d -print \
-      -o -type l -print 2> /dev/null | fzf -m | while read item
-      echo -n (echo -n "$item" | sed 's/ /\\\\ /g')' '
+      -o -type l -print 2> /dev/null
+  end
+
+  function __fzf_list_dir
+    command find * -path '*/\.*' -prune -o -type d -print 2> /dev/null
+  end
+
+  function __fzf_escape
+    while read item
+      echo -n (echo -n "$item" | sed -E 's/([ "$~'\''([{<>})])/\\\\\\1/g')' '
     end
-    echo
   end
 
   function __fzf_ctrl_t
     if [ -n "$TMUX_PANE" -a "$FZF_TMUX" != "0" ]
       tmux split-window (__fzf_tmux_height) "fish -c 'fzf_key_bindings; __fzf_ctrl_t_tmux \\$TMUX_PANE'"
     else
-      __fzf_select > $TMPDIR/fzf.result
-      and commandline -i (cat $TMPDIR/fzf.result)
+      __fzf_list | fzf -m > $TMPDIR/fzf.result
+      and commandline -i (cat $TMPDIR/fzf.result | __fzf_escape)
+      commandline -f repaint
       rm -f $TMPDIR/fzf.result
     end
   end
 
   function __fzf_ctrl_t_tmux
-    __fzf_select > $TMPDIR/fzf.result
-    and tmux send-keys -t $argv[1] (cat $TMPDIR/fzf.result)
+    __fzf_list | fzf -m > $TMPDIR/fzf.result
+    and tmux send-keys -t $argv[1] (cat $TMPDIR/fzf.result | __fzf_escape)
     rm -f $TMPDIR/fzf.result
   end
 
   function __fzf_ctrl_r
-    if history | fzf +s +m > $TMPDIR/fzf.result
-      commandline (cat $TMPDIR/fzf.result)
-    else
-      commandline -f repaint
-    end
+    history | fzf +s +m > $TMPDIR/fzf.result
+    and commandline (cat $TMPDIR/fzf.result)
+    commandline -f repaint
     rm -f $TMPDIR/fzf.result
   end
 
   function __fzf_alt_c
-    find * -path '*/\.*' -prune -o -type d -print 2> /dev/null | fzf +m > $TMPDIR/fzf.result
-    if [ (cat $TMPDIR/fzf.result | wc -l) -gt 0 ]
-      cd (cat $TMPDIR/fzf.result)
-    end
+    # Fish hangs if the command before pipe redirects (2> /dev/null)
+    __fzf_list_dir | fzf +m > $TMPDIR/fzf.result
+    [ (cat $TMPDIR/fzf.result | wc -l) -gt 0 ]
+    and cd (cat $TMPDIR/fzf.result)
     commandline -f repaint
     rm -f $TMPDIR/fzf.result
   end
