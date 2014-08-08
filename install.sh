6cccb299da95d4ae3b89b47a2a5a3dcc1192a55f
Nathan Kot <me@nathankot.com>
2014-08-09 00:53:54 +1200

    Boxen setup scripts.

--- install.sh
+++ install.sh
@@ -4,3 +4,16 @@ cd "$(dirname "${BASH_SOURCE}")"
 
 source sync.sh
 source osx.sh
+
+# Okay defer to boxen
+git submodule init
+git submodule update
+
+sudo mkdir -p /opt/boxen
+sudo chown ${USER}:staff /opt/boxen
+cd /opt/boxen
+ln -s /opt/boxen/repo "$(dirname "${BASH_SOURCE}")/boxen"
+cd /opt/boxen/repo
+
+# script/boxen
+
