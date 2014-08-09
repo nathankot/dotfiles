1ad3989bdbb7c927ff0be712aadc1772fcac3b1c
Nathan Kot <me@nathankot.com>
2014-08-09 14:47:59 +1200

    Fix up phpenv and nodenv

--- dotfiles/.shell/paths.fish
+++ dotfiles/.shell/paths.fish
@@ -31,14 +31,15 @@ set -x PATH $BOXEN_HOME/heroku/bin $PATH
 
 # PHP
 set -x PHPENV_ROOT $BOXEN_HOME/phpenv
-set -x PATH $BOXEN_HOME/phpenv/bin $BOXEN_HOME/phpenv/plugins/php-build/bin $PATH
-phpenv init
+set -x PATH $BOXEN_HOME/phpenv/shims $BOXEN_HOME/phpenv/bin $BOXEN_HOME/phpenv/plugins/php-build/bin $PATH
+phpenv rehash 2>/dev/null
 
 # NODE
 set -x NODENV_ROOT $BOXEN_HOME/nodenv
 set -x PATH $BOXEN_HOME/nodenv/bin $PATH
 set -x PATH node_modules/.bin: PATH
-nodenv init
+set -x PATH "/opt/boxen/nodenv/shims:${PATH}"
+nodenv rehash 2>/dev/null
 
 # RUBY
 set -x PATH /opt/boxen/ruby-build/bin:$PATH
