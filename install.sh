b2db60ae8c923bbe54d79db9520e1efd6dd3e2ec
Nathan Kot <me@nathankot.com>
2014-08-15 19:34:38 +1200

    Configurable BOXEN_REPO.

--- install.sh
+++ install.sh
@@ -6,10 +6,12 @@ source sync.sh
 
 # Okay defer to boxen
 
+BOXEN_REPO=${BOXEN_REPO:-'https://github.com/nathankot/our-boxen.git'}
+
 sudo mkdir -p /opt/boxen
 sudo chown ${USER}:staff /opt/boxen
 cd /opt/boxen
-git clone https://github.com/nathankot/our-boxen.git ./repo
+git clone $BOXEN_REPO ./repo
 cd /opt/boxen/repo
 
 script/boxen
