5d1ecb7c49d1add3f80c2cd5d6fc037810e06e67
Nathan Kot <me@nathankot.com>
2014-09-12 00:18:47 +1200

    No need to specify lua paths.

--- dotfiles/.shell/paths.fish
+++ dotfiles/.shell/paths.fish
@@ -19,9 +19,6 @@ set -x HOMEBREW_CACHE /opt/boxen/cache/homebrew
 set -x CFLAGS "-I$HOMEBREW_ROOT/include"
 set -x LDFLAGS "-L$HOMEBREW_ROOT/lib"
 
-set -x LUA_PATH $HOMEBREW_ROOT/lib/lua/5.2 $LUA_PATH
-set -x LUAC_PATH $HOMEBREW_ROOT/lib/lua/5.2 $LUAC_PATH
-
 if test -f $BOXEN_HOME/repo/.git
   set -x BOXEN_SETUP_VERSION (env GIT_DIR=$BOXEN_HOME/repo/.git git rev-parse --short HEAD)
 end
--- dotfiles/.shell/paths.sh
+++ dotfiles/.shell/paths.sh
@@ -2,6 +2,4 @@ if [ -f ~/opt/boxen/env.sh ]; then
   source /opt/boxen/env.sh
 fi
 
-export LUA_PATH=$HOMEBREW_ROOT/lib/lua/5.2:$LUA_PATH
-export LUAC_PATH=$HOMEBREW_ROOT/lib/lua/5.2:$LUAC_PATH
 export CHROME_BIN=/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome
