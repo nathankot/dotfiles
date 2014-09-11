118a72a5483214172ba7b80ba6a1c2bee5a20a6a
Nathan Kot <me@nathankot.com>
2014-09-11 22:48:20 +1200

    Lua path in .sh.

--- dotfiles/.shell/paths.sh
+++ dotfiles/.shell/paths.sh
@@ -2,4 +2,6 @@ if [ -f ~/opt/boxen/env.sh ]; then
   source /opt/boxen/env.sh
 fi
 
+export LUA_PATH=$HOMEBREW_ROOT/lib/luarocks:$LUA_PATH
+export LUAC_PATH=$HOMEBREW_ROOT/lib/luarocks:$LUAC_PATH
 export CHROME_BIN=/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome
