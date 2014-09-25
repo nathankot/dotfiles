4dd2cfe4a36214ed3ae610797cc2fcb31726c71f
Nathan Kot <me@nathankot.com>
2014-09-25 20:57:08 +1200

    Wrong place.

--- dotfiles/.shell/paths.fish
+++ dotfiles/.shell/paths.fish
@@ -50,8 +50,3 @@ set -x PATH $BOXEN_HOME/ruby-build/bin $PATH
 set -x RBENV_ROOT /opt/boxen/rbenv
 set -x PATH $RBENV_ROOT/shims $RBENV_ROOT/bin $PATH
 rbenv rehash >/dev/null ^&1
-
-# REDIS
-set -x BOXEN_REDIS_HOST 127.0.0.1
-set -x BOXEN_REDIS_PORT 16379
-set -x BOXEN_REDIS_URL "redis://$BOXEN_REDIS_HOST:$BOXEN_REDIS_PORT/"
