b7868697c222619f1ddc20780464f0c2e5879510
Nathan Kot <me@nathankot.com>
2014-10-27 11:25:11 +1300

    Use mjolnir to switch between secure and insecure connections.

--- dotfiles/.mjolnir/init.lua
+++ dotfiles/.mjolnir/init.lua
@@ -4,6 +4,7 @@ package.cpath = package.cpath .. ';/opt/boxen/homebrew/lib/lua/5.2/?.so'
 local hotkey = require "mjolnir.hotkey"
 local spotify = require "mjolnir.lb.spotify"
 local tiling = require "mjolnir.tiling"
+local alert = require "mjolnir.alert"
 
 local mash = {"ctrl", "cmd"}
 local musicmash = {"ctrl", "alt"}
@@ -17,3 +18,21 @@ hotkey.bind(mash, "space", function() tiling.promote() end)
 hotkey.bind(musicmash, "space", spotify.play)
 hotkey.bind(musicmash, "h", spotify.previous)
 hotkey.bind(musicmash, "l", spotify.next)
+
+-- Easily switch network location
+
+function secure()
+  os.execute('/opt/boxen/homebrew/bin/tor')
+  os.execute('networksetup -switchtolocation Secure')
+  alert.show('Using SECURE Connection', 1)
+end
+
+function insecure()
+  os.execute('killall tor')
+  os.execute('networksetup -switchtolocation Insecure')
+  alert.show('Using INSECURE Connection', 1)
+end
+
+hotkey.bind(mash, "s", function() secure() end)
+hotkey.bind(mash, "i", function() insecure() end)
+
