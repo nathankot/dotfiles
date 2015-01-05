d8f3df423962a9235e7f4a6bda65305499acd3c8
Nathan Kot <me@nathankot.com>
2015-01-05 17:33:47 +0900

    Fix boot2docker shellinit

--- dotfiles/.shell/paths.fish
+++ dotfiles/.shell/paths.fish
@@ -60,4 +60,6 @@ set -x PATH ./env/bin $PATH
 # Docker
 set -x BOOT2DOCKER_DIR /opt/boxen/data/docker
 set -x BOOT2DOCKER_PROFILE /opt/boxen/config/docker/profile
-boot2docker shellinit >/dev/null ^&1
+for line in (boot2docker shellinit 2>/dev/null)
+  eval $line
+end
