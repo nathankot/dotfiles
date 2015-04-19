ba12d5e350c559eb129a5ebcb3a0b650b81b0622
Nathan Kot <me@nathankot.com>
2015-04-20 01:40:12 +0900

    Move stuff from environments.fish to paths.fish

--- dotfiles/.shell/environments.fish
+++ dotfiles/.shell/environments.fish
@@ -4,23 +4,3 @@ set -x GITHUB_USER $USER
 # My home folder is diff to my github user
 if test $USER = 'nathan'; set -x GITHUB_USER nathankot; end
 set -x BOXEN_GITHUB_LOGIN $GITHUB_USER
-
-### Browser testing
-
-set -x CHROME_BIN /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome
-
-# Ledger
-
-set -x LEDGER ~/Documents/Personal/Accounts/current.ledger
-
-# MONGODB
-
-set -x BOXEN_MONGODB_HOST 127.0.0.1
-set -x BOXEN_MONGODB_PORT 27017
-set -x BOXEN_MONGODB_URL mongodb://$BOXEN_MONGODB_HOST:$BOXEN_MONGODB_PORT/
-
-# REDIS
-
-set -x BOXEN_REDIS_HOST 127.0.0.1
-set -x BOXEN_REDIS_PORT 16379
-set -x BOXEN_REDIS_URL "redis://$BOXEN_REDIS_HOST:$BOXEN_REDIS_PORT/"
