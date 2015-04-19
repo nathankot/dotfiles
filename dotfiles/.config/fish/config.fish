627dd970625a48b58e59d501a93803f449aabdd0
Nathan Kot <me@nathankot.com>
2015-04-20 01:44:41 +0900

    Combine shell_env into environments.fish

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -3,19 +3,15 @@ set -x SHELL /usr/local/bin/fish
 # Paths
 source ~/.shell/paths.fish
 
-# Language Environments
+# Environment
 source ~/.shell/environments.fish
 
 if test -f ~/.shell/private.fish
   source ~/.shell/private.fish
 end
 
-# Terminal Environment
-source ~/.shell/shell_env.fish
-
 # Functions
 source ~/.shell/functions.fish
 
 # Aliases
 source ~/.shell/aliases.fish
-
--- dotfiles/.shell/environments.fish
+++ dotfiles/.shell/environments.fish
@@ -1,5 +1,8 @@
-# Github
-
+set fish_greeting "Let's go."
+set -x FISH_CLIPBOARD_CMD pbcopy
+set -x TERM screen-256color
+set -x EDITOR emacs
+set -x ALTERNATIVE_EDITOR emacs # fallback to vanilla
 set -x GITHUB_USER $USER
 # My home folder is diff to my github user
 if test $USER = 'nathan'; set -x GITHUB_USER nathankot; end
