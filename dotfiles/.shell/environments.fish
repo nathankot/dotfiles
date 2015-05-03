d34fb854e357c3b93bf457295d829cc996e43dda
Nathan Kot <me@nathankot.com>
2015-05-03 10:33:17 +0900

    Add LEDGER_BASE_CURRENCY

--- dotfiles/.shell/environments.fish
+++ dotfiles/.shell/environments.fish
@@ -7,3 +7,4 @@ set -x GITHUB_USER $USER
 # My home folder is diff to my github user
 if test $USER = 'nathan'; set -x GITHUB_USER nathankot; end
 set -x BOXEN_GITHUB_LOGIN $GITHUB_USER
+set -x LEDGER_BASE_CURRENCY NZD
