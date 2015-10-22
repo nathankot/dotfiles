88eb51f637f5cd2f644d1eb8d334910128e222eb
Nathan Kot <me@nathankot.com>
2015-10-22 23:50:05 +0900

    Fix fish load paths

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -1,17 +1,9 @@
 set -x SHELL /usr/local/bin/fish
 
 # Paths
-source ~/.shell/paths.fish
-
-# Environment
-source ~/.shell/environments.fish
-
+source ~/.shell/0.environment.fish
+source ~/.shell/1.aliases.fish
+source ~/.shell/2.functions.fish
 if test -f ~/.shell/private.fish
   source ~/.shell/private.fish
 end
-
-# Functions
-source ~/.shell/functions.fish
-
-# Aliases
-source ~/.shell/aliases.fish
