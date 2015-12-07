572276cea7a4dfca118b35191da3e9119f82a501
Nathan Kot <me@nathankot.com>
2015-12-07 22:14:37 +0900

    Add stack path to bash paths

--- dotfiles/.shell/paths.sh
+++ dotfiles/.shell/paths.sh
@@ -9,3 +9,13 @@ export PATH="$HOME/bin:$PATH"
 # local bin
 export PATH="./.bin:$PATH"
 export PATH="./bin:$PATH"
+
+# haskell
+if command -v stack > /dev/null; then
+  export PATH="$HOME/.local/bin:$PATH"
+fi
+
+# virtualenv
+if command -v virtualenv > /dev/null; then
+  export PATH="./env/bin:$PATH"
+fi
