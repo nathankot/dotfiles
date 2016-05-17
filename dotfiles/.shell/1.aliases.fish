a41e40c8c4e528f52f5525616c726e8f092baf89
Nathan Kot <me@nathankot.com>
2016-05-17 15:04:46 +0900

    Revert "Revert "Use the new abbr function""
    This reverts commit 5a8851d428b6c32c35582c5141e6e2e7b9a68ba4.

--- dotfiles/.shell/1.aliases.fish
+++ dotfiles/.shell/1.aliases.fish
@@ -1,32 +1,31 @@
-set fish_user_abbreviations $fish_user_abbreviations 'l=ls'
-set fish_user_abbreviations $fish_user_abbreviations 'g=git'
-set fish_user_abbreviations $fish_user_abbreviations 'gs=git status'
-set fish_user_abbreviations $fish_user_abbreviations 'gd=git diff'
-set fish_user_abbreviations $fish_user_abbreviations 'gdc=git diff --cached'
-set fish_user_abbreviations $fish_user_abbreviations 'gc=git commit -m'
-set fish_user_abbreviations $fish_user_abbreviations 'gi=git issues'
-set fish_user_abbreviations $fish_user_abbreviations 'gl=git log'
-set fish_user_abbreviations $fish_user_abbreviations 'gca=git commit -am'
-set fish_user_abbreviations $fish_user_abbreviations 'gco=git checkout'
-set fish_user_abbreviations $fish_user_abbreviations 'ga=git add'
-set fish_user_abbreviations $fish_user_abbreviations 'gaa=git add -A'
-set fish_user_abbreviations $fish_user_abbreviations 'gp=git push'
-set fish_user_abbreviations $fish_user_abbreviations 'gim=git issues milestone'
+abbr --add l ls
+abbr --add g git
+abbr --add gs git status
+abbr --add gd git diff
+abbr --add gdc git diff --cached
+abbr --add gc git commit -m
+abbr --add gi git issues
+abbr --add gl git log
+abbr --add gca git commit -am
+abbr --add gco git checkout
+abbr --add ga git add
+abbr --add gaa git add -A
+abbr --add gp git push
 
 if command -v tmuxinator > /dev/null
-  set fish_user_abbreviations $fish_user_abbreviations 'mux=tmuxinator'
+  abbr --add mux tmuxinator
 end
 
 if command -v timetrap > /dev/null
-  set fish_user_abbreviations $fish_user_abbreviations 'ts=timetrap sheet'
-  set fish_user_abbreviations $fish_user_abbreviations 'ti=timetrap in'
-  set fish_user_abbreviations $fish_user_abbreviations 'to=timetrap out'
-  set fish_user_abbreviations $fish_user_abbreviations 'td=timetrap display'
-  set fish_user_abbreviations $fish_user_abbreviations 'tn=timetrap now'
+  abbr --add ts timetrap sheet
+  abbr --add ti timetrap in
+  abbr --add to timetrap out
+  abbr --add td timetrap display
+  abbr --add tn timetrap now
 end
 
 function legedit
   emacsclient -a "" -t ~/Documents/Personal/Accounts/current.ledger
 end
 
-alias git="hub"
+alias git hub
