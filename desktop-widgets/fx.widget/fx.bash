429cbde354a7fabaf5a16ce8fe9786d7edf92470
Nathan Kot <me@nathankot.com>
2016-10-01 10:31:14 +0900

    Remove desktop widget binaries

--- desktop-widgets/fx.widget/fx.bash
+++ desktop-widgets/fx.widget/fx.bash
@@ -1,6 +1,8 @@
 TIME_FRAME='1m'
 START_DATE=$(date -v-${TIME_FRAME} +'%Y %-m %-d')
 SYMBOLS='NZD:JPY:MYR:BTC'
+JQ='/usr/local/bin/jq'
+SPARK='/usr/local/bin/spark'
 
 read Y M D <<< ${START_DATE}
 
@@ -8,19 +10,19 @@ RESULT='[]'
 USER_AGENT='Mozilla/5.0 (Linux; Android 6.0.1; MotoG3 Build/MPI24.107-55) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.81 Mobile Safari/537.36'
 
 for SYM in `echo $SYMBOLS | tr ':' "\n"`; do
-  RESULT=$(echo $RESULT | ./bin/jq --arg n $SYM --arg t $TIME_FRAME '. + [{ symbol: $n, history: "", timeframe: $t }]')
+  RESULT=$(echo $RESULT | $JQ --arg n $SYM --arg t $TIME_FRAME '. + [{ symbol: $n, history: "", timeframe: $t }]')
   if [ "$SYM" != "BTC" ]
     then
       history=$(curl -A "$USER_AGENT" --fail -s "http://ichart.finance.yahoo.com/table.csv?s=${SYM}%3DX&g=d&ignore=.csv&c=${Y}&a=$(($M - 1))&b=${D}" \
             | tail -n +2 \
             | awk -F, '{ print $4 * 100 }' \
-            | ./bin/spark)
-      RESULT=$(echo $RESULT | ./bin/jq --arg n $SYM --arg h $history '(.[] | select(.symbol==$n) | .history) = $h')
+            | $SPARK)
+      RESULT=$(echo $RESULT | $JQ --arg n $SYM --arg h $history '(.[] | select(.symbol==$n) | .history) = $h')
     fi
 
   PRICE=$(curl -A "$USER_AGENT" --fail -s http://finance.yahoo.com/webservice/v1/symbols/${SYM}USD%3DX/quote?format=json \
-            | ./bin/jq -c '.list.resources[0].resource.fields.price | tonumber')
-  RESULT=$(echo $RESULT | ./bin/jq --arg p "$PRICE" --arg n "$SYM" '(.[] | select(.symbol==$n) | .price) = $p')
+            | $JQ -c '.list.resources[0].resource.fields.price | tonumber')
+  RESULT=$(echo $RESULT | $JQ --arg p "$PRICE" --arg n "$SYM" '(.[] | select(.symbol==$n) | .price) = $p')
 done
 
 echo $RESULT
