63fd44749f8d8d6a4635f7a9b168792e38e60cc9
Nathan Kot <me@nathankot.com>
2017-04-08 02:30:52 +1200

    Add back support for tab-shift-pad

--- dotfiles/.hammerspoon/init.lua
+++ dotfiles/.hammerspoon/init.lua
@@ -3,6 +3,15 @@ local tiling = require "hs.tiling"
 
 local mash = {"ctrl", "cmd"}
 
+local function pressFn(key)
+	return function() hs.eventtap.keyStroke({}, key, 1000) end
+end
+
+local function pressShiftFn(key)
+  mods = {}
+	return function() hs.eventtap.keyStroke({"shift"}, key, 1000) end
+end
+
 tiling.set('layouts', { 'fullscreen', 'main-vertical' })
 
 hotkey.bind(mash, "r", function() hs.reload(); hs.openConsole() end)
@@ -10,11 +19,6 @@ hotkey.bind(mash, "c", function() tiling.cycleLayout() end)
 hotkey.bind(mash, "j", function() tiling.cycle(1) end)
 hotkey.bind(mash, "k", function() tiling.cycle(-1) end)
 hotkey.bind(mash, "space", function() tiling.promote() end)
-
-local function pressFn(key)
-  mods = {}
-	return function() hs.eventtap.keyStroke(mods, key, 1000) end
-end
 
 hotkey.bind({"alt"}, "space", pressFn("0"))
 hotkey.bind({"alt"}, "n", pressFn("1"))
@@ -26,3 +30,14 @@ hotkey.bind({"alt"}, "k", pressFn("6"))
 hotkey.bind({"alt"}, "y", pressFn("7"))
 hotkey.bind({"alt"}, "u", pressFn("8"))
 hotkey.bind({"alt"}, "i", pressFn("9"))
+
+hotkey.bind({"alt", "shift"}, "space", pressShiftFn("0"))
+hotkey.bind({"alt", "shift"}, "n", pressShiftFn("1"))
+hotkey.bind({"alt", "shift"}, "m", pressShiftFn("2"))
+hotkey.bind({"alt", "shift"}, ",", pressShiftFn("3"))
+hotkey.bind({"alt", "shift"}, "h", pressShiftFn("4"))
+hotkey.bind({"alt", "shift"}, "j", pressShiftFn("5"))
+hotkey.bind({"alt", "shift"}, "k", pressShiftFn("6"))
+hotkey.bind({"alt", "shift"}, "y", pressShiftFn("7"))
+hotkey.bind({"alt", "shift"}, "u", pressShiftFn("8"))
+hotkey.bind({"alt", "shift"}, "i", pressShiftFn("9"))
