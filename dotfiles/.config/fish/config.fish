95af21e3bf0ebdf52f0a6f9411d5780357ff918b
Nathan Kot <me@nathankot.com>
2017-12-28 04:20:17 +0900

    Use new hyperfuse theme

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -1,23 +1,24 @@
 set -x SHELL /usr/local/bin/fish
 
-set fish_color_autosuggestion 262626
-set fish_color_command 5f5f87
-set fish_color_comment red
-set fish_color_cwd 6c6c6c
+set fish_color_autosuggestion normal
+set fish_color_command cyan
+set fish_color_comment brblack
+set fish_color_cwd brblack
 set fish_color_cwd_root red
-set fish_color_error 870000\x1e\x2d\x2dbold
-set fish_color_escape 5fafaf
+set fish_color_error red
+set fish_color_escape normal
 set fish_color_history_current cyan
-set fish_color_host \x2do\x1ecyan
-set fish_color_match 005f87
+set fish_color_host normal
+set fish_color_match cyan
 set fish_color_normal normal
 set fish_color_operator cyan
-set fish_color_param eeeeee
-set fish_color_quote 5f8700
-set fish_color_redirection normal
-set fish_color_search_match 262626\x1e\x2d\x2dbackground\x3dff00ff
+set fish_color_param normal
+set fish_color_quote brmagenta
+set fish_color_redirection green
+set fish_color_search_match cyan
 set fish_color_status red
-set fish_color_user \x2do\x1egreen
+set fish_color_user green
+
 set fish_color_valid_path \x2d\x2dunderline
 set fish_greeting Let\x27s\x20go\x2e
 set fish_key_bindings fish_default_key_bindings
