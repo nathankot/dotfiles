c2a8968bec9c1cabadbb6da0b6de347ba67d53ff
Nathan Kot <nathankot@users.noreply.github.com>
2018-11-17 15:27:33 -0800

    Remove stray exit

--- install.sh
+++ install.sh
@@ -8,7 +8,6 @@ FILES_DEST=${FILES_SOURCE//$PWD\/dotfiles/$HOME}
 cd "$HOME" || exit
 xargs -n 1 rm -r <<<"$FILES_DEST"
 xargs -n 1 ln -s <<<"$FILES_SOURCE"
-cd - || exit
 
 cd workstation
 ../env/bin/pip install -r ../requirements.txt
