403fa90294d19e0ef781e24e2cb832606162918b
Nathan Kot <nathankot@users.noreply.github.com>
2018-11-17 15:28:37 -0800

    Ignore rm errors

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ cd "$(dirname "${BASH_SOURCE}")"
 FILES_SOURCE=$(find "$PWD/dotfiles" -depth 1)
 FILES_DEST=${FILES_SOURCE//$PWD\/dotfiles/$HOME}
 cd "$HOME" || exit
-xargs -n 1 rm -r <<<"$FILES_DEST"
+xargs -n 1 rm -rf <<<"$FILES_DEST"
 xargs -n 1 ln -s <<<"$FILES_SOURCE"
 
 cd workstation
