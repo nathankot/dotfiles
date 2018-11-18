e603364b1850a121e4ba0753e1c8032d43f46abc
Nathan Kot <me@nathankot.com>
2018-11-17 19:19:50 -0800

    Updates after setting up a new machine

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -33,3 +33,6 @@ source ~/.shell/1.aliases.fish
 if test -f ~/.shell/private.fish
   source ~/.shell/private.fish
 end
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f "$HOME/google-cloud-sdk/path.fish.inc" ]; . "$HOME/google-cloud-sdk/path.fish.inc"; end
--- install.sh
+++ install.sh
@@ -1,15 +1,24 @@
 #!/usr/bin/env bash
 set -e
 
+# Please ensure you have the following requirements before
+# running this script:
+#
+# * Homebrew
+# * Python 2: `brew install python@2`
+# * Virtualenv: `pip install virtualenv`
+# * Env: `virtualenv env`
+
 cd "$(dirname "${BASH_SOURCE}")"
 
-FILES_SOURCE=$(find "$PWD/dotfiles" -depth 1)
-FILES_DEST=${FILES_SOURCE//$PWD\/dotfiles/$HOME}
+ROOT=$PWD
+FILES_SOURCE=$(find "$ROOT/dotfiles" -depth 1)
+FILES_DEST=${FILES_SOURCE//$ROOT\/dotfiles/$HOME}
 cd "$HOME" || exit
 xargs -n 1 rm -rf <<<"$FILES_DEST"
 xargs -n 1 ln -s <<<"$FILES_SOURCE"
 
-cd "$(dirname "${BASH_SOURCE}")/workstation"
+cd "$ROOT/workstation"
 ../env/bin/pip install -r ../requirements.txt
 ../env/bin/ansible-playbook install.yml -e "github_access_token=$GITHUB_API_TOKEN"
 cd - || exit
