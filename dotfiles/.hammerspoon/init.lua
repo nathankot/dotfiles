17fcc5a7bcd5bdcdb1bc9dcdf380c3b4ea3ade67
Nathan Kot <me@nathankot.com>
2020-10-02 15:34:39 +0800

    Try out alacritty

--- dotfiles/.hammerspoon/init.lua
+++ dotfiles/.hammerspoon/init.lua
@@ -15,3 +15,11 @@ hotkey.bind(tilingMash, "c", function() tiling.cycleLayout() end)
 hotkey.bind(tilingMash, "j", function() tiling.cycle(1) end)
 hotkey.bind(tilingMash, "k", function() tiling.cycle(-1) end)
 hotkey.bind(tilingMash, "space", function() tiling.promote() end)
+hotkey.bind({"ctrl"}, "return", function()
+  local alacritty = hs.application.find('alacritty')
+  if alacritty:isFrontmost() then
+    alacritty:hide()
+  else
+    hs.application.launchOrFocus("/Applications/Alacritty.app")
+  end
+end)
