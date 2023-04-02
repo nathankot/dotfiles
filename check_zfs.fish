9f57e9e4e1f89b65b3eaa837358fea017772ae7d
Nathan Kot <me@nathankot.com>
2023-04-02 22:46:31 +0800

    Check smart status

--- check_zfs.fish
+++ check_zfs.fish
@@ -3,4 +3,8 @@ if nc -z z.local 22
   if ssh root@z.local zpool status | grep state: | grep -v ONLINE
     osascript -e 'display notification "ZFS pool on Z in bad state" with title "Z.local"'
   end
+
+  if not ssh root@z.local /root/check_smart_status.bash
+    osascript -e 'display notification "One of the drives in the ZFS pool are unhealthy" with title "Z.local"'
+  end
 end
