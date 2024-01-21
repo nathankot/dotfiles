a725b1c000fdfd967160a7af552e66f539a3b354
Nathan Kot <me@nathankot.com>
2024-01-21 10:44:48 +0800

    Update zfs check script

--- check_zfs.fish
+++ check_zfs.fish
@@ -1,6 +1,6 @@
 #!/usr/bin/env fish
-if nc -z z.local 22
-  if ssh root@z.local zpool status | grep state: | grep -v ONLINE
+if nc -z nas.local 22
+  if ssh nathan@nas.local zpool status | grep state: | grep -v ONLINE
     osascript -e 'display notification "ZFS pool on Z in bad state" with title "Z.local"'
   end
 
