fe6544dcd71ebed026b18a6fcdc686aa5c359a16
Nathan Kot <me@nathankot.com>
2026-06-01 11:42:52 +0900

    Fix asdf path

--- home/.shell/0.environment.fish
+++ home/.shell/0.environment.fish
@@ -49,6 +49,20 @@ set -x PATH node_modules/.bin $PATH
 
 if command -v asdf > /dev/null
   asdf completion fish > ~/.config/fish/completions/asdf.fish
+
+  # ASDF configuration code
+  if test -z $ASDF_DATA_DIR
+      set _asdf_shims "$HOME/.asdf/shims"
+  else
+      set _asdf_shims "$ASDF_DATA_DIR/shims"
+  end
+
+  # Do not use fish_add_path (added in Fish 3.2) because it
+  # potentially changes the order of items in PATH
+  if not contains $_asdf_shims $PATH
+      set -gx --prepend PATH $_asdf_shims
+  end
+  set --erase _asdf_shims
 end
 
 set -x PATH ./env/bin $PATH
--- home/.shell/paths.sh
+++ home/.shell/paths.sh
@@ -45,17 +45,5 @@ fi
 export PATH=node_modules/.bin:$PATH
 
 if command -v asdf > /dev/null; then
-  # ASDF configuration code
-  if test -z $ASDF_DATA_DIR
-      set _asdf_shims "$HOME/.asdf/shims"
-  else
-      set _asdf_shims "$ASDF_DATA_DIR/shims"
-  end
-
-  # Do not use fish_add_path (added in Fish 3.2) because it
-  # potentially changes the order of items in PATH
-  if not contains $_asdf_shims $PATH
-      set -gx --prepend PATH $_asdf_shims
-  end
-  set --erase _asdf_shims
+  export PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"
 fi
